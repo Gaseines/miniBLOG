f7fab3055c662a5f8638d4e04170009698ad3777
Gaseines <gaseinesdzn@hotmail.com>
2025-01-07 22:06:14 -0300

    iniciando projeto
    Criando navbar e footer, navegação entre as paginas, adicionando a config do firebase

---
+++ miniblog/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+//CSS
+import styles from './Navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <NavLink className={styles.brand} to={"/"}>
+        mini <span>Blog</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink to={"/"} className={({isActive}) => (isActive ? styles.active : '')}>Home</NavLink>
+        </li>
+        <li>
+          {" "}
+          <NavLink to={"/about"} className={({isActive}) => (isActive ? styles.active : '')}>Sobre</NavLink>{" "}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ miniblog/src/firebase/config.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcWTyUlJx06C6bG7CrD1TQKax65K-pg-o",
+  authDomain: "miniblog-893d9.firebaseapp.com",
+  projectId: "miniblog-893d9",
+  storageBucket: "miniblog-893d9.firebasestorage.app",
+  messagingSenderId: "1015342990034",
+  appId: "1:1015342990034:web:02d3a0dee5c0e824b8230b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db }
